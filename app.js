2d781c337b80bbc5c3eddf45f169a1e2728ef623
Michael Eskin <seisiuneer@users.noreply.github.com>
2023-02-13 08:21:11 -0800

    Switched to QRCode.CorrectLevel.M and max URL of 2300 characters

--- app.js
+++ app.js
@@ -1690,7 +1690,7 @@ function FillUrlBoxWithAbcInBase64() {
 	} else {
 
 		// If fits in a QR code, show the QR code button
-		var maxURLLength = 1650;
+		var maxURLLength = 2300;
 	
 		if (url.length < maxURLLength) {
 
@@ -1737,7 +1737,7 @@ function FillUrlBoxWithAbcInLZW() {
 	} else {
 
 		// If fits in a QR code, show the QR code button
-		var maxURLLength = 1650;
+		var maxURLLength = 2300;
 	
 		if (url.length < maxURLLength) {
 
@@ -1794,7 +1794,7 @@ function GenerateQRCode() {
 			height: 512,
 			colorDark: "#000000",
 			colorLight: "#ffffff",
-    		correctLevel : QRCode.CorrectLevel.Q
+    		correctLevel : QRCode.CorrectLevel.M
 		});
 
 	} else {
