e89c312c8e290882c72a180acd636104bfe2ef58
Michael Eskin <seisiuneer@users.noreply.github.com>
2023-07-19 13:48:14 -0700

    Added credit

--- tools/box-tab.js
+++ tools/box-tab.js
@@ -9,6 +9,8 @@
 // Michael Eskin
 // http://michaeleskin.com
 //
+// ABC parsing algorithm by James van Donsel
+//
 // Released under CC0 - No Rights Reserved
 // https://creativecommons.org/share-your-work/public-domain/cc0/
 //
