0e38b36d7544abc5ab5c0aedd0fd89086c1bcccb
Michael Eskin <seisiuneer@users.noreply.github.com>
2023-07-25 14:33:39 -0700

    Update anglo-fingerings.js

--- tools/anglo-fingerings.js
+++ tools/anglo-fingerings.js
@@ -850,8 +850,26 @@ function getAbcNotes(input) {
 
         }
 
-
     }
+
+    // Sanitize in-abc chords in brackets
+    searchRegExp = /\[[^\]|]*\]/g
+
+    while (m = searchRegExp.exec(sanitizedInput)) {
+
+
+        var start = m.index;
+        var end = start + m[0].length;
+
+        //console.log(m[0],start,end);
+
+        for (var index=start;index<end;++index){
+
+            sanitizedInput = sanitizedInput.substring(0, index) + '*' + sanitizedInput.substring(index + 1);
+
+        }
+
+    }  
     
     log("sanitized input:" + sanitizedInput);
 
