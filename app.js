ae1254b022e3c89e66ca496d061bbedcb90d0f8f
Michael Eskin <seisiuneer@users.noreply.github.com>
2023-08-27 17:44:43 -0700

    Improved play after paste behavior

--- app.js
+++ app.js
@@ -12707,12 +12707,18 @@ function DoStartup() {
 	// the text change logic only re-renders if the number of tunes has changed
 	// because of a text entry event
 	//
-	// document.getElementById('abc').onpaste = 
-	// 	function(){
+	document.getElementById('abc').onpaste = 
+		function(){
 			
-	// 		//gForceFullRender = true;
+			// Refocus back on the ABC
+			setTimeout(function(){
+				gTheABC.focus();
+				gTheABC.selectionStart = gTheABC.selectionStart-1;
+	    		gTheABC.selectionEnd = gTheABC.selectionStart;
 
-	// 	};
+			},250);
+
+		};
 
 	//
 	// Hook up the text area text change callback with debounce
