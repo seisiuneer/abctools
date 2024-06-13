bce9b0ad996d30861124209bf64b2f60f249bb92
Michael Eskin <seisiuneer@gmail.com>
2024-06-13 07:29:29 -0700

    Added a better MusicXML title capitalizer

--- app.js
+++ app.js
@@ -36439,6 +36439,60 @@ function InjectQTag(theTune,theTempo){
 
 }
 
+/*
+ * Title Caps
+ * 
+ * Ported to JavaScript By John Resig - http://ejohn.org/ - 21 May 2008
+ * Original by John Gruber - http://daringfireball.net/ - 10 May 2008
+ * License: http://www.opensource.org/licenses/mit-license.php
+ */
+
+function doTitleCaps(title){
+
+	var small = "(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v[.]?|via|vs[.]?)";
+	var punct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
+  
+	function titleCaps(title){
+		var parts = [], split = /[:.;?!] |(?: |^)["Ò]/g, index = 0;
+		
+		while (true) {
+			var m = split.exec(title);
+
+			parts.push( title.substring(index, m ? m.index : title.length)
+				.replace(/\b([A-Za-z][a-z.'Õ]*)\b/g, function(all){
+					return /[A-Za-z]\.[A-Za-z]/.test(all) ? all : upper(all);
+				})
+				.replace(RegExp("\\b" + small + "\\b", "ig"), lower)
+				.replace(RegExp("^" + punct + small + "\\b", "ig"), function(all, punct, word){
+					return punct + upper(word);
+				})
+				.replace(RegExp("\\b" + small + punct + "$", "ig"), upper));
+			
+			index = split.lastIndex;
+			
+			if ( m ) parts.push( m[0] );
+			else break;
+		}
+		
+		return parts.join("").replace(/ V(s?)\. /ig, " v$1. ")
+			.replace(/(['Õ])S\b/ig, "$1s")
+			.replace(/\b(AT&T|Q&A)\b/ig, function(all){
+				return all.toUpperCase();
+			});
+	};
+    
+	function lower(word){
+		return word.toLowerCase();
+	}
+    
+	function upper(word){
+	  return word.substr(0,1).toUpperCase() + word.substr(1);
+	}
+
+	return titleCaps(title);
+
+}
+
 //
 // Import MusicXML format
 //
@@ -36486,19 +36540,8 @@ function importMusicXML(theXML,fileName){
 		fileName = fileName.replaceAll("_"," ");
 		fileName = fileName.replaceAll("-"," ");
 
-		// Init cap all the words
-		fileName = fileName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
-
-		// Lower case connecting words in the middle of the title
-		fileName = fileName.replaceAll(" And "," and ");
-		fileName = fileName.replaceAll(" Or "," or ");
-		fileName = fileName.replaceAll(" The "," the ");
-		fileName = fileName.replaceAll(" To "," to ");
-		fileName = fileName.replaceAll(" In "," in ");
-		fileName = fileName.replaceAll(" On "," on ");
-		fileName = fileName.replaceAll(" From "," from ");
-		fileName = fileName.replaceAll(" Of "," of ");
-
+		// Intelligent title capitalize
+		fileName = doTitleCaps(fileName);
 
     	abcText = abcText.replace("T:Title","T:"+fileName);
     }
