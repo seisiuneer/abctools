9df79b73818a2b190dafffd1da78f67866b7ce96
Michael Eskin <seisiuneer@gmail.com>
2024-09-11 17:26:32 -0700

    Start of unification of standard and offline versions

--- app/always-online.js
+++
@@ -1,6 +0,0 @@
-// Assume always online (to avoid having to load piano and percussion in all cases)
-function doOnlineCheck(){
-	gSamplesOnline = true;
-	//console.log("always-online.js - gSamplesOnLine = "+gSamplesOnline);
-}
-
