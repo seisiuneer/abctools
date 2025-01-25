e772cdd13d8726c1dbdba2c59c799aae2eff3540
Michael Eskin <seisiuneer@gmail.com>
2025-01-25 08:20:47 -0800

    Switched to en dash before the number for draw indication

--- tools/harmonica_tab.js
+++ tools/harmonica_tab.js
@@ -204,46 +204,46 @@ function generate_harmonica_tab(tuneABC,harpKey,octaveShift){
     tab[0] = "x"; // unknown
 
     tab[1] = "1"; // C
-    tab[2] = "1'-"; // C# / Db
-    tab[3] = "1-"; // D
+    tab[2] = "–1'"; // C# / Db
+    tab[3] = "–1"; // D
     tab[4] = "1o"; // E# / Eb
     tab[5] = "2"; // E
-    tab[6] = "2''-"; // F
-    tab[7] = "2'-"; // F# / Gb
+    tab[6] = "–2''"; // F
+    tab[7] = "–2'"; // F# / Gb
     tab[8] = "3"; // G
-    tab[9] = "3'''-"; // G# / Ab
-    tab[10] = "3''-"; // A
-    tab[11] = "3'-"; // A# / Bb
-    tab[12] = "3-"; // B
+    tab[9] = "–3'''"; // G# / Ab
+    tab[10] = "–3''"; // A
+    tab[11] = "–3'"; // A# / Bb
+    tab[12] = "–3"; // B
 
     tab[13] = "4"; // C
-    tab[14] = "4'-"; // C# / Db
-    tab[15] = "4-"; // D
+    tab[14] = "–4'"; // C# / Db
+    tab[15] = "–4"; // D
     tab[16] = "4o"; // E# / Eb
     tab[17] = "5"; // E
-    tab[18] = "5-"; // F
+    tab[18] = "–5"; // F
     tab[19] = "5o"; // F# / Gb
     tab[20] = "6"; // G
-    tab[21] = "6'-"; // G# / Ab
-    tab[22] = "6-"; // A
+    tab[21] = "–6'"; // G# / Ab
+    tab[22] = "–6"; // A
     tab[23] = "6o"; // A# / Bb
-    tab[24] = "7-"; // B
+    tab[24] = "–7"; // B
 
     tab[25] = "7"; // C
-    tab[26] = "7o-"; // C# / Db
-    tab[27] = "8-"; // D
+    tab[26] = "–7o"; // C# / Db
+    tab[27] = "–8"; // D
     tab[28] = "8'"; // E# / Eb
     tab[29] = "8"; // E
-    tab[30] = "9-"; // F
+    tab[30] = "–9"; // F
     tab[31] = "9'"; // F# / Gb
     tab[32] = "9"; // G
-    tab[33] = "9o-"; // G# / Ab
-    tab[34] = "10-"; // A
+    tab[33] = "–9o"; // G# / Ab
+    tab[34] = "–10"; // A
     tab[35] = "10''"; // A# / Bb
     tab[36] = "10'"; // B
 
     tab[37] = "10"; // C
-    tab[38] = "10o-'"; // C#
+    tab[38] = "–10o'"; // C#
 
     // Set up defaults
     let keySig = "C";
