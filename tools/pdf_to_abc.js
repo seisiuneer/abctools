454c85ce852d72d5937f2171d0df15e9cd01873d
Michael Eskin <seisiuneer@gmail.com>
2025-01-29 17:21:14 -0800

    Added pdf_to_abc utility for extracting tune ABC from PDF tunebooks with share links.

---
+++ tools/pdf_to_abc.js
@@ -0,0 +1,519 @@
+//
+// pdf_to_abc.js
+//
+// ABC Tools PDF to ABC Extraction Tool
+//
+//
+// Released under CC0 - No Rights Reserved
+// https://creativecommons.org/share-your-work/public-domain/cc0/
+//
+
+//
+// Save the Output to a .abc file
+//
+function saveOutput() {
+
+    var theData = document.getElementById("output").value;
+
+    if (theData.length == 0) {
+
+        DayPilot.Modal.alert("Nothing to save!", {
+            theme: "modal_flat",
+            top: 50
+        });
+
+        return;
+    }
+
+    if (gSaveFilename == "") {
+        gSaveFilename = "harmonica_tab";
+    }
+
+    var thePlaceholder = gSaveFilename;
+
+
+    var thePrompt = "Please enter a filename for your output ABC file:";
+
+    DayPilot.Modal.prompt(thePrompt, thePlaceholder, {
+        theme: "modal_flat",
+        top: 194,
+        autoFocus: false
+    }).then(function(args) {
+
+        var fname = args.result;
+
+        // If the user pressed Cancel, exit
+        if (fname == null) {
+            return null;
+        }
+
+        // Strip out any naughty HTML tag characters
+        fname = fname.replace(/[^a-zA-Z0-9_\-. ]+/ig, '');
+
+        if (fname.length == 0) {
+            return null;
+        }
+
+        // Give it a good extension
+        if ((!gIsAndroid) && (!gIsIOS)) {
+
+            if ((!fname.endsWith(".abc")) && (!fname.endsWith(".txt")) && (!fname.endsWith(".ABC")) && (!fname.endsWith(".TXT"))) {
+
+                // Give it a good extension
+                fname = fname.replace(/\..+$/, '');
+                fname = fname + ".abc";
+
+            }
+        } else {
+            // iOS and Android have odd rules about text file saving
+            // Give it a good extension
+            fname = fname.replace(/\..+$/, '');
+            fname = fname + ".txt";
+
+        }
+
+        var a = document.createElement("a");
+
+        document.body.appendChild(a);
+
+        a.style = "display: none";
+
+        var blob = new Blob([theData], {
+                type: "text/plain"
+            }),
+
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fname;
+        a.click();
+
+        document.body.removeChild(a);
+
+        setTimeout(function() {
+            window.URL.revokeObjectURL(url);
+        }, 1000);
+
+    });
+
+}
+
+
+//
+// Copy the output text are to the clipboard 
+// 
+function copyOutputToClipboard() {
+
+    var textToCopy = document.getElementById('output').value;
+
+    if (textToCopy.length == 0) {
+
+        DayPilot.Modal.alert("Nothing to copy!", {
+            theme: "modal_flat",
+            top: 50
+        });
+
+        return;
+
+    }
+
+    copyToClipboard(textToCopy);
+
+    // Give some feedback
+    document.getElementById("copybutton").innerHTML = "Output copied to the clipboard!";
+
+    setTimeout(function() {
+
+        document.getElementById("copybutton").innerHTML = "Copy output to the clipboard";
+
+    }, 1250);
+}
+
+//
+// Copy to Clipboard Polyfill
+//
+function copyToClipboard(textToCopy) {
+
+    //
+    // Put this in a try/catch just to be safe
+    //
+    try {
+
+        // navigator clipboard api needs a secure context (https)
+        if (navigator.clipboard && window.isSecureContext) {
+
+            // navigator clipboard api method'
+            return navigator.clipboard.writeText(textToCopy);
+
+        } else {
+
+            // text area method
+
+            let textArea = document.createElement("textarea");
+
+            textArea.value = textToCopy;
+
+            // make the textarea out of viewport
+            textArea.style.position = "fixed";
+            textArea.style.left = "-999999px";
+            textArea.style.top = "-999999px";
+
+            document.body.appendChild(textArea);
+
+            textArea.focus();
+            textArea.select();
+
+            return new Promise((res, rej) => {
+                // here the magic happens
+                document.execCommand('copy') ? res() : rej();
+                textArea.remove();
+            });
+        }
+
+    } catch (error) {
+
+        console.log("CopyToClipboard error: " + error);
+
+    }
+}
+
+//
+// Get the current base URL
+//
+function getUrlWithoutParams() {
+
+    return "https://michaeleskin.com/abctools/abctools.html";
+
+}
+
+//
+// Generate and open an ABC Transcription Tools Share URL for the output
+//
+function testOutput() {
+
+    // Encode all the tunes or just what's passed in?
+    var abcText = document.getElementById('output').value;
+
+    if (abcText.length == 0) {
+
+        DayPilot.Modal.alert("Nothing to test!", {
+            theme: "modal_flat",
+            top: 50
+        });
+
+        return;
+
+    }
+
+    var shareName = "PDF_to_ABC_Test";
+
+    var abcInLZW = LZString.compressToEncodedURIComponent(abcText);
+
+    var url = "https://michaeleskin.com/abctools/abctools.html?lzw=" + abcInLZW + "&format=noten&ssp=45&pdf=one&pn=br&fp=yes&btfs=10&name=" + shareName + "&editor=1";
+
+    if (url.length > 8100) {
+
+        DayPilot.Modal.alert("Output too long to test!", {
+            theme: "modal_flat",
+            top: 50
+        });
+
+        return;
+    }
+
+    // Open the transcription tools with the share link
+    var w = window.open(url);
+
+}
+
+var LZString=function(){function o(o,r){if(!t[o]){t[o]={};for(var n=0;n<o.length;n++)t[o][o.charAt(n)]=n}return t[o][r]}var r=String.fromCharCode,n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",t={},i={compressToBase64:function(o){if(null==o)return"";var r=i._compress(o,6,function(o){return n.charAt(o)});switch(r.length%4){default:case 0:return r;case 1:return r+"===";case 2:return r+"==";case 3:return r+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(e){return o(n,r.charAt(e))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(o){return null==o?"":""==o?null:i._decompress(o.length,16384,function(r){return o.charCodeAt(r)-32})},compressToUint8Array:function(o){for(var r=i.compress(o),n=new Uint8Array(2*r.length),e=0,t=r.length;t>e;e++){var s=r.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null===o||void 0===o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;t>e;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(o){return null==o?"":i._compress(o,6,function(o){return e.charAt(o)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(n){return o(e,r.charAt(n))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")},decompress:function(o){return null==o?"":""==o?null:i._decompress(o.length,32768,function(r){return o.charCodeAt(r)})},_decompress:function(o,n,e){var t,i,s,p,u,c,a,l,f=[],h=4,d=4,m=3,v="",w=[],A={val:e(0),position:n,index:1};for(i=0;3>i;i+=1)f[i]=i;for(p=0,c=Math.pow(2,2),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(t=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 2:return""}for(f[3]=l,s=l,w.push(l);;){if(A.index>o)return"";for(p=0,c=Math.pow(2,m),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(l=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 2:return w.join("")}if(0==h&&(h=Math.pow(2,m),m++),f[l])v=f[l];else{if(l!==d)return null;v=s+s.charAt(0)}w.push(v),f[d++]=s+v.charAt(0),h--,s=v,0==h&&(h=Math.pow(2,m),m++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module&&(module.exports=LZString);
+
+
+function extractLZWParameter(url) {
+   // Use a regular expression to find the part starting with &lzw= followed by any characters until the next &
+    const match = url.match(/lzw=([^&]*)/);
+
+    // If a match is found, return the part after &lzw=
+    return match ? match[0] : null;
+}
+
+//
+// Are we on iOS?
+//
+function isIOS() {
+    if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+        return true;
+    } else {
+        return navigator.maxTouchPoints &&
+            navigator.maxTouchPoints > 2 &&
+            /MacIntel/.test(navigator.platform);
+    }
+}
+
+//
+// Are we on an iPhone?
+//
+function isIPhone() {
+    if (/iPad|iPhone|iPod/.test(navigator.platform)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//
+// Are we on an iPad?
+//
+function isIPad() {
+    return navigator.maxTouchPoints &&
+        navigator.maxTouchPoints > 2 &&
+        /MacIntel/.test(navigator.platform);
+}
+
+//
+// Are we on Android?
+//
+function isAndroid() {
+    if (/Android/i.test(navigator.userAgent)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//
+// Are we on Safari?
+//
+function isSafari() {
+
+    if (/Safari/i.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//
+// Are we on Chrome?
+//
+function isChrome() {
+
+    if (/chrome|chromium|crios/i.test(navigator.userAgent)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//
+// Globals
+//
+var gIsIOS = false;
+var gIsAndroid = false;
+var gIsSafari = false;
+var gIsChrome = false;
+
+//
+// Initialization 
+//
+function DoStartup() {
+
+    document.getElementById('output').value = "";
+
+    // Reset file selectors
+    var fileElement = document.getElementById('selectpdffile');
+
+    fileElement.value = "";
+
+    // Are we on iOS?
+    gIsIOS = false;
+    if (isIOS()) {
+        gIsIOS = true;
+    }
+
+    // Are we on Android?
+    gIsAndroid = false;
+
+    if (isAndroid()) {
+        gIsAndroid = true;
+    }
+
+    if (gIsIOS) {
+        document.getElementById("selectpdffile").removeAttribute("accept");
+    }
+
+    // Are we on Safari?
+    gIsSafari = false;
+    if (isSafari()) {
+        gIsSafari = true;
+    }
+
+    // Are we on Chrome?
+    gIsChrome = false;
+
+    if (!gIsSafari) {
+        if (isChrome()) {
+            gIsChrome = true;
+        }
+    }
+
+    document.getElementById('selectpdffile').addEventListener('change', async (event) => {
+
+        //debugger;
+
+        const file = event.target.files[0];
+
+        if (file) {
+
+            //debugger;
+
+            // Read the file as an ArrayBuffer
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(file);
+
+            reader.onload = async function() {
+
+                const {
+                    PDFDocument,
+                    PDFName,
+                    PDFDict
+                } = PDFLib;
+
+                const pdfBytes = reader.result;
+
+                // Load the PDF with pdf-lib
+                const pdfDoc = await PDFDocument.load(pdfBytes);
+
+                const links = [];
+
+                // Loop through all pages in the PDF
+                const totalPages = pdfDoc.getPageCount();
+
+                for (let pageIndex = 0; pageIndex < totalPages; pageIndex++) {
+
+                    const page = pdfDoc.getPage(pageIndex);
+                    
+                    const annotations = page.node.Annots();
+
+                    if (annotations) {
+
+                        annotations.array.forEach((annot) => {
+
+                            if (annot.dict){
+
+                                const annotationDict = annot.dict.get(PDFName.of('A'));
+
+                                if (annotationDict) {
+
+                                    const uri = annotationDict.lookup(PDFName.of('URI'));
+
+                                    if (uri) {
+
+                                        var theLink = uri.toString();
+                                        
+                                        // Some PDF files have odd delimiters and splits in the links
+
+                                        theLink = theLink.replaceAll("\\","");
+                                        theLink = theLink.replaceAll("\r","");
+                                        theLink = theLink.replaceAll("\r","");
+
+                                        // Remove the wrapping parenthesis
+                                        theLink = theLink.slice(1, -1);
+
+                                        links.push(theLink);
+                                    }
+                                }
+                            }
+
+                        });
+                    }
+                }
+
+                // Display the extracted links
+                const output = document.getElementById('output');
+
+                var nFound = 0;
+
+                var outVal = "";
+
+                if (links.length > 0) {
+
+                    links.forEach(link => {
+
+                        if (link.indexOf("lzw=") != -1){
+
+                            var originalAbcInLZW = extractLZWParameter(link);
+
+                            originalAbcInLZW = originalAbcInLZW.replace("lzw=","");
+                        
+                            var abcInLZW = LZString.decompressFromEncodedURIComponent(originalAbcInLZW);
+
+                            // Sometimes the LZW decompress fails if the link is too long for the PDF or some other issue
+                            if (abcInLZW){
+
+                                nFound++;
+
+                                outVal += `${abcInLZW}\n\n`;
+
+                            }
+
+                        }
+
+                    });
+                }
+
+                if (nFound == 0){
+
+                    outVal = '%\n% No tunes found in the PDF.\n%\n';
+
+                }
+                else{
+
+                    if (nFound == 1){
+                        outVal = "%\n% "+nFound+" tune found in "+file.name+"\n%\n\n"+outVal;
+                    }
+                    else{
+                        outVal = "%\n% "+nFound+" tunes found in "+file.name+"\n%\n\n"+outVal; 
+                    }
+
+                }
+
+                // Strip extra linefeeds
+                outVal = outVal.replaceAll("\n\n\n","\n\n");
+
+                output.value = outVal;
+
+                // Reset file selectors
+                let fileElement = document.getElementById('selectpdffile');
+
+                fileElement.value = "";
+
+            };
+
+            reader.onerror = function() {
+                console.error('Error reading file');
+            };
+        }
+    });
+
+}
+
+//
+// Wait for the document to be ready, then fire a function
+//
+
+function WaitForReady(fn) {
+
+    if (document.readyState !== 'loading') {
+        fn();
+        return;
+    }
+
+    document.addEventListener('DOMContentLoaded', fn);
+
+}
+
+//
+// Wait for the document to be ready, then startup
+//
+
+WaitForReady(DoStartup);
