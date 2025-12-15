fafe5347af48fd4c6be1590c959d5e9f540841b7
Michael Eskin <seisiuneer@gmail.com>
2025-12-15 08:55:38 -0800

    Volume scaling for fiddle

--- abcjs-basic-eskin.js
+++ abcjs-basic-eskin.js
@@ -18645,7 +18645,7 @@ function CreateSynth(theABC) {
         "whistle": 2.0,     // 78 
         "banjo": 2.0,       // 105
         "bagpipe":3.0,     // 109 
-        "fiddle": 5.0,      // 110
+        "fiddle": 5.5,      // 110
         "melodic_tom": 3.25,  // 117
         "uilleann": 1.75,    // 129
         "smallpipesd": 1.75, // 130
