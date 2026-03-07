2750f1df220e5de8657a3f7850118ec8b371341e
Michael Eskin <seisiuneer@gmail.com>
2026-03-07 08:16:36 -0800

    Date change on "What's New"

--- app.js
+++ app.js
@@ -55619,7 +55619,7 @@ function showWhatsNewScreen() {
   modal_msg += 'background: linear-gradient(135deg, #1b5e20 0%, #2e7d32 50%, #66bb6a 100%);';
   modal_msg += 'box-shadow: 0 6px 16px rgba(0,0,0,0.14); color:#fff;">';
   modal_msg += '<div style="font-size:20pt; line-height:24pt; font-weight:bold;">What&apos;s New</div>';
-  modal_msg += '<div style="font-size:11pt; opacity:0.92; margin-top:3px;">Version ' + gVersionNumber + ' released 6 March 2026</div>';
+  modal_msg += '<div style="font-size:11pt; opacity:0.92; margin-top:3px;">Version ' + gVersionNumber + ' released 7 March 2026</div>';
   modal_msg += '</div>';
 
   // Short intro
