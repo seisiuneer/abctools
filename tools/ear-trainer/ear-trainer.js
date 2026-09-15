5ec04d07f692e6b5cac439512ae6eba8fd3d1e4f
Michael Eskin <seisiuneer@gmail.com>
2026-09-15 12:22:08 -0700

    Updated the swing values on jigs, slip jigs, and slides.

--- tools/ear-trainer/ear-trainer.js
+++ tools/ear-trainer/ear-trainer.js
@@ -1,7 +1,7 @@
 (function(){
 "use strict";
 
-var VERSION="2.14";
+var VERSION="2.15";
 var FATBOY="https://michaeleskin.com/abctools/soundfonts/fatboy_4/";
 var SESSION_LENGTH=10;
 var ANSWER_STYLE_STORAGE_KEY="keyModeEarTrainerAnswerStyle";
