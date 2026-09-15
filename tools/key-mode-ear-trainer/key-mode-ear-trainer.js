da93652eb40f3462a44954b94df398a0ac949098
Michael Eskin <seisiuneer@gmail.com>
2026-09-14 20:23:02 -0700

    Removed "The Earl's Chair"

--- tools/key-mode-ear-trainer/key-mode-ear-trainer.js
+++ tools/key-mode-ear-trainer/key-mode-ear-trainer.js
@@ -1,7 +1,7 @@
 (function(){
 "use strict";
 
-var VERSION="1.77";
+var VERSION="1.78";
 var FATBOY="https://michaeleskin.com/abctools/soundfonts/fatboy_4/";
 var SESSION_LENGTH=10;
 var MODE_INFO={
